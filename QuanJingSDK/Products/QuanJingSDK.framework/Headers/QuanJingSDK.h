7d29bb053ed1417b8c19ac1fa2416fa37899158b
gongtao <gongtao@quanjing.com>
2015-10-20 19:22:24 +0800

    修改SDK

---
+++ QuanJingSDK/Products/QuanJingSDK.framework/Headers/QuanJingSDK.h
@@ -0,0 +1,25 @@
+//
+//  QuanJingSDK.h
+//  QuanJingSDK
+//
+//  Created by QJ on 15/10/18.
+//  Copyright © 2015年 QJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#ifndef _QUANJINGSDK_
+  #define _QUANJINGSDK_
+  
+// ! Project version number for QuanJingSDK.
+	FOUNDATION_EXPORT double QuanJingSDKVersionNumber;
+	
+// ! Project version string for QuanJingSDK.
+	FOUNDATION_EXPORT const unsigned char QuanJingSDKVersionString[];
+  #import "QJPassport.h"
+  #import "QJImageObject.h"
+  #import "QJImageCategory.h"
+  #import "QJHomeIndexObject.h"
+  #import "QJActionObject.h"
+  #import "QJInterfaceManager.h"
+#endif
